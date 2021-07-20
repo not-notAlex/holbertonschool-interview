5aee45d0f18a85028416e84d8df82ca5af503399
Alex Smith <1763@holbertonschool.com>
2021-07-20 06:21:59 +0000

    answer and readme

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+module for lockboxes challenge
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    returns True if all lockboxes can be opened
+    """
+    opened = [0]
+    for x in opened:
+        for y in boxes[x]:
+            if y < len(boxes) and not y in opened:
+                opened.append(y)
+    if len(opened) == len(boxes):
+        return True
+    else:
+        return False
