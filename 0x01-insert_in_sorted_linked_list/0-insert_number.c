8d30be07a3663284237bf39d1eef4ec6dfc63dbb
Alex Smith <1763@holbertonschool.com>
2021-07-27 20:09:50 +0000

    answer dump

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -1,10 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
 /**
  * name - insert_node
  * @head: head of list 
- * @number: index of insertion
+ * @number: value of new node
  * Return: pointer to new node
  */
 listint_t *insert_node(listint_t **head, int number)
 {
+  listint_t *node, *temp;
+  
+    if (head == NULL)
+    {
+        return (NULL);
+    }
 
+    node = malloc(sizeof(listint_t));
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    node->n = number;
+
+    if (*head == NULL)
+    {
+        *head = node;
+	node->next = NULL;
+        return node;
+    }
+
+    temp = *head;
+    while (temp->next->n < number)
+        temp = temp->next;
+    node->next = temp->next;
+    temp->next = node;
+    return (node);
 }
