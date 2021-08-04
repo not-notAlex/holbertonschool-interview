c8d675a94002e4955cbdce324eed9394e9f952cf
Alex Smith <1763@holbertonschool.com>
2021-08-04 21:33:58 +0000

    initial answers to avoid emacs ~

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - inserts a value into a max binary heap
+ * @root: root node of heap
+ * @value: value to store in node
+ * Return: new inserted node
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+
+}
+
+binary_tree_t find_node(binary_tree_t *root)
+{
+    if (root->left == NULL && root->right == NULL)
+        return (NULL);
+
+    
+}
