b3c3d4c3cf1eb05df5b8d18bc8f0d39911724070
Alex Smith <1763@holbertonschool.com>
2021-08-05 20:38:24 +0000

    fixed betty style

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -8,14 +8,14 @@
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *node;
+	binary_tree_t *node;
 
-    node = malloc(sizeof(binary_tree_t));
-    if (node == NULL)
-        return (NULL);
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    node->n = value;
-    return (node);
+	node = malloc(sizeof(binary_tree_t));
+	if (node == NULL)
+		return (NULL);
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	node->n = value;
+	return (node);
 }
--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -8,77 +8,90 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *node, *tar;
-    int max = 0, temp = 0;
+	heap_t *node, *tar;
+	int max = 0, temp = 0;
 
-    node = binary_tree_node(NULL, value);
-    if (node == NULL)
-        return (NULL);
-    node->n = value;
-    node->left = NULL;
-    node->right = NULL;
-    if (*root == NULL)
-    {
-	*root = node;
-	return (node);
-    }
-    max = maxDepth(*root);
-    tar = find_node(*root, 0, max - 1);
-    if (tar == NULL)
-    {
-        tar = *root;
-        while (tar->left)
-	    tar = tar->left;
-    }
-    node->parent = tar;
-    if (tar->left == NULL)
-        tar->left = node;
-    else
-        tar->right = node;
-    while(node->parent)
-    {
-        if (node->n > node->parent->n)
+	node = binary_tree_node(NULL, value);
+	if (node == NULL)
+		return (NULL);
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	if (*root == NULL)
 	{
-	    temp = node->n;
-	    node->n = node->parent->n;
-	    node->parent->n = temp;
-	    node = node->parent;
+		*root = node;
+		return (node);
 	}
-        else
-	    break;
-    }
-    return (node);
+	max = maxDepth(*root);
+	tar = find_node(*root, 0, max - 1);
+	if (tar == NULL)
+	{
+		tar = *root;
+		while (tar->left)
+			tar = tar->left;
+	}
+	node->parent = tar;
+	if (tar->left == NULL)
+		tar->left = node;
+	else
+		tar->right = node;
+	while (node->parent)
+	{
+		if (node->n > node->parent->n)
+		{
+			temp = node->n;
+			node->n = node->parent->n;
+			node->parent->n = temp;
+			node = node->parent;
+		}
+		else
+			break;
+	}
+	return (node);
 }
 
+/**
+ * maxDepth - returns depth of tree
+ * @node: tree to search
+ *
+ * Return: depth
+ */
 int maxDepth(heap_t *node)
 {
-    int l, r;
-  
-    if (node == NULL)
-        return (0);
-    l = maxDepth(node->left);
-    r = maxDepth(node->right);
-    if (l > r)
-        return (l + 1);
-    else
-        return (r + 1);
+	int l, r;
+
+	if (node == NULL)
+		return (0);
+	l = maxDepth(node->left);
+	r = maxDepth(node->right);
+	if (l > r)
+		return (l + 1);
+	else
+		return (r + 1);
 }
 
+/**
+ * find_node - finds the node for new value to insert
+ * @root: root of tree to search
+ * @depth: current depth into tree
+ * @max: max depth of the tree
+ * Return: node to insert or NULL if tree is perfect
+ */
 heap_t *find_node(heap_t *root, int depth, int max)
 {
-    heap_t *l, *r;
+	heap_t *l, *r;
 
-    if (depth == max)
-        return (NULL);
-    if (root->left != NULL && root->right == NULL)
-        return (root);
-    if (root->left == NULL && depth != max)
-        return (root);
-    
-    l = find_node(root->left, depth + 1, max);
-    r = find_node(root->right, depth + 1, max);
-    if (l != NULL)
-        return (l);
-    else
-        return (r);
+	if (depth == max)
+		return (NULL);
+	if (root->left != NULL && root->right == NULL)
+		return (root);
+	if (root->left == NULL && depth != max)
+		return (root);
+
+	l = find_node(root->left, depth + 1, max);
+	r = find_node(root->right, depth + 1, max);
+	if (l != NULL)
+		return (l);
+	else
+		return (r);
 }
