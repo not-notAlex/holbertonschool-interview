86b8e4bd97cb790d2e3210cb514f68d93e638c2a
Alex Smith <1763@holbertonschool.com>
2021-08-12 05:37:57 +0000

    initial answer

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import math
+
+
+def minOperations(n):
+    """
+    finds the minimum operations
+    """
+    prime_factors = []
+    i = 3
+    c = n
+    if n == 1 or n <= 0:
+        return 0
+    if n == 2:
+        return 2
+    if n == 3:
+        return 3
+    while i <= c:
+        if c % 2 == 0:
+            c = c / 2
+            prime_factors.append(2)
+        if c % i == 0:
+            c = c / i
+            prime_factors.append(i)
+            i = 3
+        if c == 2:
+            prime_factors.append(2)
+        i = i + 2
+    return sum(prime_factors)
