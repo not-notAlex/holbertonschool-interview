780e3fe4c29a0ffa0a664d5555b47a04991be3ad
Alex Smith <1763@holbertonschool.com>
2021-08-24 19:48:40 +0000

    initial commit

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - determines if list is palindrome
+ * @head: head of the list
+ *
+ * Return: 0 if false 1 if true
+ */
+int is_palindrome(listint_t **head)
+{
+
+}
