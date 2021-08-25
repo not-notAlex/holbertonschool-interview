d90486514f01af3028106f116056340d4237a1ce
Alex Smith <1763@holbertonschool.com>
2021-08-25 20:53:49 +0000

    answer

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * is_palindrome - determines if list is palindrome
@@ -8,5 +10,35 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *tmp = *head;
+	int len = 0, *lst, i = 0;
 
+	while (tmp)
+	{
+		len++;
+		tmp = tmp->next;
+	}
+	if (len < 2)
+		return (1);
+	lst = malloc(sizeof(int) * len);
+	if (lst == NULL)
+		return (0);
+	tmp = *head;
+	while (tmp)
+	{
+		*lst = tmp->n;
+		tmp = tmp->next;
+		lst++;
+	}
+	lst--;
+	tmp = *head;
+	while (i < (len / 2))
+	{
+		if (tmp->n != *lst)
+			return (0);
+		tmp = tmp->next;
+		lst--;
+		i++;
+	}
+	return (1);
 }
