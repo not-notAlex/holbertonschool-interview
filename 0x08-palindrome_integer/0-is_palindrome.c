047b4c442428812b666b60ec4a28a76c2615b8fe
Alex Smith <1763@holbertonschool.com>
2021-09-15 21:09:42 +0000

    answer

---
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,36 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if an int is a palindrome
+ * @n: number to check
+ *
+ * Return: 1 if true 0 if false
+ */
+int is_palindrome(unsigned long n)
+{
+	int len = 0, x;
+	unsigned long int i = n, t = 1;
+
+	while (i > 0)
+	{
+		i /= 10;
+		len++;
+	}
+	if (len == 1)
+		return (1);
+	for (x = 1; x < len; x++)
+	{
+		t *= 10;
+	}
+	x = 1;
+	i = n;
+	while (x <= (len / 2))
+	{
+		if ((i % 10) != ((i / t) % 10))
+			return (0);
+		x++;
+		t /= 10;
+		i /= 10;
+	}
+	return (1);
+}
---
+++ 0x08-palindrome_integer/palindrome.h
@@ -0,0 +1,9 @@
+#ifndef PALINDROME_H_
+#define PALINDROME_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int is_palindrome(unsigned long n);
+
+#endif
