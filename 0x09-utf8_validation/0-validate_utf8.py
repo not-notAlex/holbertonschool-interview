754e8530b327659e729ee1458f12835d77c5d7f8
Alex Smith <1763@holbertonschool.com>
2021-09-22 20:04:32 +0000

    answer

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+module for UTF-8 Validation
+"""
+
+
+def validUTF8(data):
+    """
+    determines if list is UTF-8 valid
+    """
+    i = 0
+    while(i < len(data)):
+        if data[i] >> 7 == 0:
+            i += 1
+            continue
+        mask1 = 1 << 7
+        nbytes = 0
+        while(mask1 & data[i]):
+            nbytes += 1
+            mask1 = mask1 >> 1
+        if nbytes == 0:
+            return False
+        j = 0
+        while(j < nbytes):
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            if not (data[i] & mask1 and not (data[i] & mask2)):
+                return False
+            i += 1
+            j += 1
+        i += 1
+    return True
