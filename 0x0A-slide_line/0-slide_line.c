39b9574bb3a012c698328a6365e4d98c662f1da0
Alex Smith <1763@holbertonschool.com>
2021-09-27 20:57:28 +0000

    initial commit

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,13 @@
+#include "slide_line.h"
+
+/**
+ * slide_line - slides elements in array and combines same
+ * @line: pointer to line
+ * @size: size of array
+ * @direction: the direction to slide the line
+ * Return: 1 on success 0 on failure
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+	return (0);
+}
