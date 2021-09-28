74e77a8d87d1588c9ca592ce52f13c0ae91140d5
Alex Smith <1763@holbertonschool.com>
2021-09-28 21:42:21 +0000

    answer

--- 0x0A-slide_line/0-slide_line.c
+++ 0x0A-slide_line/0-slide_line.c
@@ -9,5 +9,80 @@
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	return (0);
+	if (size < 2)
+		return (1);
+	if (direction == SLIDE_LEFT)
+		return (slide_line_left(line, size));
+	else
+		return (slide_line_right(line, size));
+}
+
+/**
+ * slide_line_left - slides line to the left
+ * @line: pointer to line
+ * @size: size of array
+ * Return: 1 on success 0 on failure
+ */
+int slide_line_left(int *line, size_t size)
+{
+	size_t i, x, merge = 0;
+
+	for (i = 1; i < size; i++)
+	{
+		if (line[i] == 0)
+			continue;
+		for (x = i; x > 0; x--)
+		{
+			if (line[x - 1] != 0)
+			{
+				if (line[x - 1] == line[x] && merge == 0)
+				{
+					line[x - 1] *= 2;
+					line[x] = 0;
+					merge = 1;
+					break;
+				}
+				merge = 0;
+				break;
+			}
+			line[x - 1] = line[x];
+			line[x] = 0;
+		}
+	}
+	return (1);
+}
+
+/**
+ * slide_line_right - slides line to the right
+ * @line: pointer to line
+ * @size: size of array
+ * Return: 1 on success 0 on failure
+ */
+int slide_line_right(int *line, size_t size)
+{
+	long int i, x, merge = 0;
+
+	for (i = size - 2; i >= 0; i--)
+	{
+		if (line[i] == 0)
+			continue;
+		for (x = i; x < size - 1; x++)
+		{
+			if (line[x + 1] != 0)
+			{
+				if (line[x + 1] == line[x] && merge == 0)
+				{
+					line[x + 1] *= 2;
+					line[x] = 0;
+					merge = 1;
+					break;
+				}
+				merge = 0;
+				break;
+			}
+			line[x + 1] = line[x];
+			line[x] = 0;
+		}
+	}
+	return (1);
 }
--- 0x0A-slide_line/slide_line.h
+++ 0x0A-slide_line/slide_line.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 int slide_line(int *line, size_t size, int direction);
+int slide_line_left(int *line, size_t size);
+int slide_line_right(int *line, size_t size);
 #define SLIDE_LEFT  -1
 #define SLIDE_RIGHT  1
 
