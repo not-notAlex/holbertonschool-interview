870fe8707fb70aa89b506e873c5a014c3bf13ade
Alex Smith <1763@holbertonschool.com>
2021-10-06 21:45:00 +0000

    answer

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -2,5 +2,28 @@
 
 void menger(int level)
 {
-	
+	int x, y, sides = 3, p, z;
+
+	if (level == 0)
+	{
+		printf("#\n");
+		return;
+	}
+	for (x = 1; x < level; x++)
+		sides *= 3;
+	for (x = 0; x < sides; x++)
+	{
+		for (y = 0; y < sides; y++)
+		{
+			p = 1;
+			for (z = 1; z < sides; z *= 3)
+				if (((x / z) % 3) == 1 && ((y / z) % 3) == 1)
+					p = 0;
+			if (p)
+				printf("#");
+			else
+				printf(" ");
+		}
+		printf("\n");
+	}
 }
