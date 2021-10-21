08b102c5eb6b9ac5670913a90c5d8b731431588a
Alex Smith <1763@holbertonschool.com>
2021-10-21 18:50:05 +0000

    answer

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -4,6 +4,57 @@ module for task
 """
 
 from sys import argv
+grid = []
+results = []
+
+
+def addResult(cords):
+    """
+    adds answer to list
+    """
+    global results
+    res = []
+    for x in range(len(grid)):
+        for y in range(len(grid)):
+            if cords[x][y]:
+                res.append([x, y])
+    if res not in results:
+        results.append(res)
+
+
+def nqueens(n):
+    """
+    solves the nqueens problem
+    """
+    global grid
+    count = 0
+    for x in range(n):
+        for y in range(n):
+            if grid[x][y] == 0:
+                if canPlace(x, y):
+                    grid[x][y] = 1
+                    nqueens(n)
+                    grid[x][y] = 0
+            else:
+                count += 1
+    if count == len(grid):
+        addResult(grid)
+
+
+def canPlace(x, y):
+    """
+    checks if you can place a queen on grid
+    """
+    global grid
+    for i in range(len(grid)):
+        if grid[x][i] or grid[i][y]:
+            return False
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if (i + j == x + y) or (i - j == x - y):
+                if grid[i][j]:
+                    return False
+    return True
 
 
 if __name__ == "__main__":
@@ -13,16 +64,19 @@ if __name__ == "__main__":
     if len(argv) != 2:
         print("Usage: nqueens N")
         exit(1)
-    if type(argv[1]) is not int:
+    try:
+        n = int(argv[1])
+    except Exception as e:
         print("N must be a number")
         exit(1)
-    if int(argv[1]) < 4:
+    if n < 4:
         print("N must be at least 4")
         exit(1)
-    nqueens(int(argv[1]))
-
-def nqueens(n):
-    """
-    solves the nqueen problem
-    """
-    print("yo")
+    for i in range(n):
+        row = []
+        for j in range(n):
+            row.append(0)
+        grid.append(row)
+    nqueens(n)
+    for i in results:
+        print(i)
