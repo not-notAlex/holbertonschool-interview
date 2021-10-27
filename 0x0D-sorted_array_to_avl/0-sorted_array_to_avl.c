a80d9377b8bd86bcdadce4db58f80f82ddd539b7
Alex Smith <1763@holbertonschool.com>
2021-10-27 18:54:59 +0000

    initial commit

---
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * sorted_array_to_avl - turns array into AVL tree
+ * @array: array to convert
+ * @size: size of the array
+ * Return: head of tree NULL on failure
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	return (NULL);
+}
---
+++ 0x0D-sorted_array_to_avl/README.md
@@ -0,0 +1,1 @@
+# AVL Tree From Array
