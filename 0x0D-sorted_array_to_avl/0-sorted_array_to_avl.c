5b9f1aa6ff09a00cdedf223036bdd2fa586f7e5c
Alex Smith <1763@holbertonschool.com>
2021-10-28 18:25:26 +0000

    answer

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,5 +8,46 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-	return (NULL);
+	unsigned int i;
+	avl_t *tree = NULL;
+
+	if (size < 1 || array == NULL)
+		return (NULL);
+
+	i = add_nodes(array, size, &tree, 1);
+	if (i)
+		return (NULL);
+	return (tree);
+}
+
+int add_nodes(int *array, size_t size, avl_t **tree, int add_left)
+{
+	size_t halfway = (size - 1) / 2;
+	avl_t *node = NULL;
+	int i = 0;
+
+	if (array == NULL || size < 1)
+		return (0);
+
+	node = malloc(sizeof(avl_t));
+	if (node == NULL)
+		return (-1);
+	node->n = array[halfway];
+	node->parent = (*tree);
+	node->left = NULL;
+	node->right = NULL;
+	if ((*tree) == NULL)
+		(*tree) = node;
+	else if (add_left)
+		(*tree)->left = node;
+	else
+		(*tree)->right = node;
+	i = add_nodes(array, halfway, &node, 1);
+	if (i == -1)
+		return (-1);
+	halfway++;
+	i = add_nodes(&array[halfway], size - halfway, &node, 0);
+	if (i == -1)
+		return (-1);
+	return (0);
 }
--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -25,5 +25,6 @@ typedef struct binary_tree_s avl_t;
 
 void binary_tree_print(const binary_tree_t *);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+int add_nodes(int *array, size_t size, avl_t **tree, int add_left);
 
 #endif
