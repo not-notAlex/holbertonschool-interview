459b1b1b77dca111075068518db2b4c730ee836c
Alex Smith <1763@holbertonschool.com>
2021-11-17 15:57:25 +0000

    answer

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -8,4 +8,24 @@ def rain(walls):
     """
     returns how many square units of water will be retained
     """
-    pass
+    if len(walls) == 0:
+        return 0
+    total = 0
+    x = 0
+    while x < len(walls):
+        if walls[x] == 0:
+            x += 1
+            continue
+        y = x + 1
+        if y >= len(walls):
+            return total
+        while walls[y] == 0:
+            y += 1
+            if y >= len(walls):
+                return total
+        if walls[x] < walls[y]:
+            width = walls[x]
+        else:
+            width = walls[y]
+        total += (y - x - 1) * width
+        x += 1
