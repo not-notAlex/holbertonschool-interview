8d1130b38f15d348d59f2bee1c60b9e8313894bd
Alex Smith <1763@holbertonschool.com>
2021-11-29 16:41:56 +0000

    first commit

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,13 @@
+#include "search_algos.c"
+
+/**
+ * advanced_binary - performs a binary search on an array
+ * @array: array to search
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index found or -1 if not
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	
+}
