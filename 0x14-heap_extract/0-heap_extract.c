92c67db1eb33cb8e12ea5688a9fd7ba0b8257a0b
Alex Smith <1763@holbertonschool.com>
2021-12-14 19:14:49 +0000

    initial commit

---
+++ 0x14-heap_extract/0-heap_extract.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a max binary heap
+ * @root: root node of the heap
+ *
+ * Return: value in node return 0 if fail
+ */
+int heap_extract(heap_t **root)
+{
+
+}
