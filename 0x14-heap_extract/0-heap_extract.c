5bf6b9d07b935fa27d7c310185478f7b4d1c22dd
Alex Smith <1763@holbertonschool.com>
2021-12-16 21:10:09 +0000

    answer

--- 0x14-heap_extract/0-heap_extract.c
+++ 0x14-heap_extract/0-heap_extract.c
@@ -8,5 +8,84 @@
  */
 int heap_extract(heap_t **root)
 {
+	int value, n;
+	size_t level = 0;
+	heap_t *node, *del;
 
+	if (root == NULL || *root == NULL)
+		return (0);
+	node = *root, value = node->n;
+	if (node->left == NULL && node->right == NULL)
+	{
+		*root = NULL;
+		free(node);
+		return (value);
+	}
+	binary_tree_preorder(node, &del, binary_tree_height(node), level);
+	while (node->left || node->right)
+	{
+		if (node->right == NULL || node->left->n > node->right->n)
+		{
+			n = node->n, node->n = node->left->n;
+			node->left->n = n;
+			node = node->left;
+		}
+		else if (node->left == NULL || node->right->n > node->left->n)
+		{
+			n = node->n, node->n = node->right->n;
+			node->right->n = n;
+			node = node->right;
+		}
+	}
+	node->n = del->n;
+	if (del->parent->right)
+		del->parent->right = NULL;
+	else
+		del->parent->left = NULL;
+	free(del);
+	return (value);
+}
+
+/**
+ * binary_tree_height - determines height of tree
+ * @tree: tree to investigate
+ *
+ * Return: szie of the tree
+ */
+int binary_tree_height(const heap_t *tree)
+{
+	int l = 0;
+	int r = 0;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return (0);
+	if (tree->left)
+		l = binary_tree_height(tree->left);
+	if (tree->right)
+		r = binary_tree_height(tree->right);
+
+	if (l >= r)
+		return (1 + l);
+	return (1 + r);
+}
+
+/**
+ * binary_tree_preorder - preorder traversal through tree
+ * @tree: said tree
+ * @node: node in the tree
+ * @h: height of tree
+ * @level: layer in the tree
+ * Return: no return
+ */
+void binary_tree_preorder(heap_t *tree, heap_t **node, size_t h, size_t level)
+{
+	if (tree == NULL)
+		return;
+	if (h == level)
+		*node = tree;
+	level++;
+	if (tree->left)
+		binary_tree_preorder(tree->left, node, h, level);
+	if (tree->right)
+		binary_tree_preorder(tree->right, node, h, level);
 }
--- 0x14-heap_extract/binary_trees.h
+++ 0x14-heap_extract/binary_trees.h
@@ -1,6 +1,11 @@
 #ifndef _BINARY_TREES_H_
 #define _BINARY_TREES_H_
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -9,15 +14,20 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
+
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 int heap_extract(heap_t **root);
+void binary_tree_preorder(heap_t *tree, heap_t **node, size_t h, size_t level);
+int binary_tree_height(const heap_t *root);
 
 #endif
