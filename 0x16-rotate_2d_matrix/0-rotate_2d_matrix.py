eb2226e0428ebdfdbff2280163899733926904ac
Alex Smith <1763@holbertonschool.com>
2022-01-05 23:13:36 +0000

    answer

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module for task 0
+"""
+
+def rotate_2d_matrix(matrix):
+    """
+    rotates a 2D matrix clockwise
+    """
+    n = len(matrix)
+    for i in range(0, int(n / 2)):
+        for j in range(i, n - i - 1):
+            tmp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = tmp
