36993ea234668c1c7c6140dc90af014923182ea2
Alex Smith <1763@holbertonschool.com>
2022-01-20 13:18:31 +0000

    initial commit

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * merge_sort - Uses merge sort on array
+ * @array: list to sort
+ * @size: size of array
+ * Return: no return
+ */
+void merge_sort(int *array, size_t size)
+{
+
+}
