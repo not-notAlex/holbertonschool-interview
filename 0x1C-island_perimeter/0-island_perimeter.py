45d5b6368b5a2b7bb642766b175d51c04908242c
Alex Smith <1763@holbertonschool.com>
2022-02-18 03:27:28 +0000

    answer

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module for task 0
+"""
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island in grid
+    """
+    result = 0
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if (y+1) < len(grid[0]) and grid[x][y+1] == 0:
+                    result += 1
+                if y == (len(grid[0]) - 1):
+                    result += 1
+                if y == 0:
+                    result += 1
+                if grid[x][y-1] == 0 and y > 0:
+                    result += 1
+                if grid[x-1][y] == 0 and x > 0:
+                    result += 1
+                if x == 0:
+                    result += 1
+                if (x+1) < len(grid) and grid[x+1][y] == 0:
+                    result += 1
+                if x == len(grid) - 1:
+                    result += 1
+    return result
